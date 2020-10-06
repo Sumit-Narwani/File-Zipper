002e0859668eba2277471df4f3f93a0f563f583c
Sumit Narwani <narwani95@gmail.com>
2020-10-06 14:25:01 +0530

    Added encoder and decoder

--- decoder.js
+++ decoder.js
@@ -1,8 +1,7 @@
 function decode(data){
-    // Splitting string into huffman tree, padding, encoded text
     data = data.split('\n');
-    if(data.length === 4){
-        // Handling new line in huffman tree
+    if(data.length===4){
+        // Handling new line
         data[0] = data[0] + '\n' + data[1];
         data[1] = data[2];
         data[2] = data[3];
@@ -10,51 +9,38 @@ function decode(data){
     }
 
     this.ind = 0;
-
-    // Decoding Huffman tree
-    const huffman_encoder = this.destrigify(data[0]);
-
-    // data[2] contains the encoded text
+    const huffman_decoder = this.destringify(data[0]);
     const text = data[2];
 
-    // Encoded text to binary string
     let binary_string = "";
-    for(let i=0; i<text.length; i++){
-
-        // Converting character to binary string
+    for(let i=0;i<text.length;i++){
         let num = text[i].charCodeAt(0);
         let bin = "";
-        for(let j=0; j<8; j++){
+        for(let j=0;j<8;j++){
             bin = num%2 + bin;
             num = Math.floor(num/2);
         }
         binary_string = binary_string + bin;
     }
+    binary_string = binary_string.substring(0,binary_string.length-data[1]);
+
+    console.log(binary_string.length);
+
+    let res = "";
+    let node = huffman_decoder;
+    for(let i=0;i<binary_string.length;i++){
+        if(binary_string[i]==='0'){
+            node = node[0];
+        } else{
+            node = node[1];
+        }
+
+        if(typeof(node[0])==="string"){
+            res += node[0];
+            node = huffman_decoder;
+        }
+    }
+    let info = "Decompression complete and file sent for download";
+    return [res, this.display(huffman_decoder, true), info];
 }
 
-
-// Removing padding
-binary_string = binary_string.substring(0, binary_string.length - data[1]);
-
-
-// Binary string to original text using huffman tree    
-let res = "";
-let node = huffman_decoder;
-for(let i=0; i<binary_string.length; i++){
-    if(binary_string[i] === '0'){
-        node = node[0];
-    }
-    else {
-        node = node[1];
-    }
-
-    if(typeof(node[0]) === 'string'){
-        res += node[0];
-        node = huffman_decoder;
-    }
-}
-
-let info = "Decompression complete and file sent for download";
-
-// Returning decoded text, tree structure, extra info
-return [res, this.display(huffman_decoder, true), info];
--- encoder.js
+++ encoder.js
@@ -1,80 +1,56 @@
 function encode(data){
-    // Get Heap
+
     this.heap = new BinaryHeap();
 
-    // store frequency count
     const mp = new Map();
     for(let i=0;i<data.length;i++){
         if(data[i] in mp){
             mp[data[i]] = mp[data[i]] + 1;
-        }
-        else{
-            mp[data[i]] = 1
+        } else{
+            mp[data[i]] = 1;
         }
     }
-
-
-    // Insert elements to heap
-    // -neg because we need min heap
 
     for(const key in mp){
         this.heap.insert([-mp[key], key]);
     }
 
-
-    // Creating Huffman tree
     while(this.heap.size() > 1){
         const node1 = this.heap.extractMax();
         const node2 = this.heap.extractMax();
-        
-        const node = [[node1[0] + node2[0], [node1, node2]];
+
+        const node = [node1[0]+node2[0],[node1,node2]];
         this.heap.insert(node);
-
     }
-
-    // Extracting Huffman tree
     const huffman_encoder = this.heap.extractMax();
 
-
-    // Get character to Binary string mappings
     this.mappings = {};
     this.getMappings(huffman_encoder, "");
 
-
-    // Mapping character string to binary string
     let binary_string = "";
-    for(let i=0; i<data.length; i++){
+    for(let i=0;i<data.length;i++) {
         binary_string = binary_string + this.mappings[data[i]];
     }
 
-
-    // Padding binary string to make length multiple of 8
-    let rem = ( 8 - binary_string.length % 8 ) % 8;
+    let rem = (8 - binary_string.length%8)%8;
     let padding = "";
-    for(let i=0; i<rem; i++){
+    for(let i=0;i<rem;i++)
         padding = padding + "0";
-        binary_string = binary_string + padding;
-    }
+    binary_string = binary_string + padding;
 
-    
-    // Binary string to corresponding array
     let result = "";
-    for(let i=0; i<binary_string.length; i++){
+    for(let i=0;i<binary_string.length;i+=8){
         let num = 0;
-        for(let j=0; j<8; j++){
-            num = num*2 + (binary_string[i+j] - "0");
+        for(let j=0;j<8;j++){
+            num = num*2 + (binary_string[i+j]-"0");
         }
         result = result + String.fromCharCode(num);
     }
 
-    // Concatenating required info to decode the tree
     let final_res = this.stringify(huffman_encoder) + '\n' + rem + '\n' + result;
-    let info = "Compression Ratio:" + data.length/final_res.length;
+    let info = "Compression Ratio : " + data.length/final_res.length;
     info = "Compression complete and file sent for download" + '\n' + info;
-
-    // Returning encoded data, tree structure, extra info
     return [final_res, this.display(huffman_encoder, false), info];
-
 }
 
 
